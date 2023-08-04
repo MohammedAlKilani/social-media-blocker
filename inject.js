0648d1051a7aedab7d2c73585d069480056cd606
Muhammad Shaker Kasem AlKilany <m.sh64944@gmail.com>
2023-08-04 17:06:06 +0300

    final twitter

--- inject.js
+++ inject.js
@@ -1,66 +1,114 @@
 
- chrome.storage.sync.get("block",function(data){
-    console.log(data.block)
-    if(data.block){
-       while(location.hostname == "www.facebook.com"){
 
-        alert("go back")
-        }  
-
-       }  
-
-chrome.storage.sync.get("dateEndTime",function(data){
-   
-    let dateEndTime =new Date(data.dateEndTime) 
-   
     
+       
+chrome.storage.sync.get("dateEndTime",dateEndTimeFun)
 
+function dateEndTimeFun (data){
    
+  
+ 
+  
 
-    if(dateEndTime){
- let i =1 
-        setInterval(()=>{ 
-            let nowTime = new Date()
-          let seconds =  dateEndTime.getSeconds() - nowTime.getSeconds() 
-          let minutes =  dateEndTime.getMinutes() - nowTime.getMinutes()
+  chrome.storage.sync.get("ArryUrlBloker",(data)=>{
+    
+      if(Array.isArray(data.ArryUrlBloker)){
+          var ArryUrlBloker = data.ArryUrlBloker
+           for (let url of ArryUrlBloker) {
+              var isBloked = (location.href == url||location.hostname == url )
+              
+            }
         
-          
-          let hours =  dateEndTime.getHours() - nowTime.getHours()
-            if(seconds<0){
-                --minutes
-                seconds +=60
-                
-            }
-             if(minutes<0){
-                
-                minutes +=60
-                --hours
-            }
-            if(hours<0){
-                chrome.storage.sync.set({"block":false})
-                chrome.storage.sync.set({"clocDown":"you can connect to social media block is over"})
-                
+      }  
+
+chrome.storage.sync.get("block",(data)=>{
+             console.log(data.block)
+            let isReload = localStorage.getItem("isReload")
+            if((!data.block)&&(!isReload)&&(isBloked)){
+              localStorage.setItem("isReload","true")
+                location.reload()
+              
             }else{
-                 
-                chrome.storage.sync.set({"block":true})
-                chrome.storage.sync.set({"clocDown":`${hours } : ${minutes } : ${seconds}`})
+              localStorage.removeItem("isReload")
+            }
+            // while( isBloked&&data.block){
+
+            //     alert("go back")
+            //     } 
+            console.log(isBloked&&data.block)
+            window.onload = ()=>{
+              if(isBloked&&data.block){
+             let elemP = document.createElement("p")
+             document.documentElement.innerHTML=""
+              elemP.innerHTML = "go back"
+              document.documentElement.style.background = "#765827"
+              elemP.style.textAlign = "center"
+              elemP.style.fontSize = "150px"
+              elemP.style.color = "#E7B10A"
+              elemP.style.fontFamily="sans-serif"
+              document.documentElement.appendChild(elemP)
+            }
             }
             
-
-            i++  
-        },1000)
+            
+          })
+ 
+  })
+  if(data){
+  let dateEndTime =new Date(data.dateEndTime) 
+  if(dateEndTime){
+let i =1 
+          let nowTime = new Date()
+        let seconds =  dateEndTime.getSeconds() - nowTime.getSeconds() 
+        let minutes =  dateEndTime.getMinutes() - nowTime.getMinutes()
+      
         
+        let hours =  dateEndTime.getHours() - nowTime.getHours()
+        let data =  dateEndTime.getDate() - nowTime.getHours()
+        let Month =  dateEndTime.getMonth() - nowTime.getMonth()
+        let Year =  dateEndTime.getFullYear() - nowTime.getFullYear()
+          if(seconds<0){
+              --minutes
+              seconds +=60
+              
+          }
+           if(minutes<0){
+              
+              minutes+=60
+              --hours
+          }
+          if(hours<0&&data==0&&Month==0&&Year==0){
+              chrome.storage.sync.set({"block":false})
+              
+              
+              
+          }
+          
 
-    }
-   
-    })
-   
-   
+          i++  
+      
+      
 
+  } 
+}
+}
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  
+  console.log(changes)
+  if(changes.dateEndTime?.newValue){
+    
+    let data={}
+    
+    data.dateEndTime = changes.dateEndTime.newValue
+    console.log(data)
+      dateEndTimeFun(data)
+  }else{
+
+    dateEndTimeFun(false)
+  }
+  
+  
 })
-
-
-
 // while(location.hostname == "www.facebook.com"){
 
 // alert("go back")
@@ -68,3 +116,13 @@ chrome.storage.sync.get("dateEndTime",function(data){
   
 
 // alert("hi i am muhammed")onBeforeRequest.addListener(()=>{},{url:["https://www.facebook.com/"]},["blocking"])
+// chrome.storage.onChanged.addListener((changes, namespace) => {
+//   console.log(changes.dateEndTime.newValue)
+//     for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+//       console.log(
+//         `Storage key "${key}" in namespace "${namespace}" changed.`,
+//         `Old value was "${oldValue}", new value is "${newValue}".`
+//       );
+//     }
+//   });
+  console.log("test")
--- poup.js
+++ poup.js
@@ -1,8 +1,76 @@
 
  let clocDown = document.getElementById("clocDown")
- clocDown.style.width ="50px"
- setInterval(()=>{chrome.storage.sync.get("clocDown",(data)=>{
-    
-   clocDown.innerHTML = data.clocDown
+ clocDown.style.width ="fitcontent"
+ chrome.storage.sync.get("block",(data)=>{
+    console.log(data)
+
+ if(data.block){
+chrome.storage.sync.get("dateEndTime",function(data){
+   
+  let dateEndTime =new Date(data.dateEndTime) 
+ console.log(dateEndTime.getFullYear()- new Date().getFullYear()==1||dateEndTime.getDate()- new Date().getDate()==1 || dateEndTime.getMonth()- new Date().getMonth()==1)
+  let add24 =0
+if(dateEndTime.getFullYear()- new Date().getFullYear()==1||dateEndTime.getDate()- new Date().getDate()==1 || dateEndTime.getMonth()- new Date().getMonth()==1){
+    add24=24
+}
+
  
- })},1000)
+
+  if(dateEndTime){
+
+let i =1 
+     let stopInterval  = setInterval(()=>{ 
+          let nowTime = new Date()
+        let seconds =  dateEndTime.getSeconds() - nowTime.getSeconds() 
+        let minutes =  dateEndTime.getMinutes() - nowTime.getMinutes()
+      
+        
+        let hours =  dateEndTime.getHours()+add24 - nowTime.getHours()
+          if(seconds<0){
+              --minutes
+              seconds +=60
+              
+          }
+           if(minutes<0){
+              
+              minutes +=60
+              --hours
+          }
+          if(hours<0){
+              chrome.storage.sync.set({"block":false})
+              clearInterval(stopInterval)
+              clocDown.innerHTML = "you can connect to social media block is over"
+              
+          }else{
+               
+              // chrome.storage.sync.set({"block":true})
+              clocDown.innerHTML = `${hours } : ${minutes } : ${seconds}`
+          }
+          
+
+          i++  
+      },1000)
+      
+
+  }
+ 
+  })
+ }else{
+    clocDown.innerHTML = "you can connect to social media block is over" 
+ }
+     
+   
+ 
+})
+
+$("input[type=time]").on("change",()=>{
+ let  dateEndTime =new Date(new Date().toDateString()+" "+$("input[type=time]").val()) 
+ if((dateEndTime- new Date())<0){
+  let day = new Date().getDate()
+  dateEndTime.setDate(++day)
+ }
+ console.log(dateEndTime) 
+  chrome.storage.sync.set({"block":true})
+  chrome.storage.sync.set({ "dateEndTime":`${dateEndTime}`})
+  // close()
+})
